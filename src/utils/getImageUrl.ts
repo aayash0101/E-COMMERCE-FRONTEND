e95d277a3b4710d3c9f4745fa4657ebb633b5b49
Aayash <dangaayash@gmail.com>
2026-07-09 17:11:31 +0545

    get Image

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,9 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL as string;
-
-export const SERVER_ORIGIN = API_BASE.replace(/\/api\/v1\/?$/, "");
-
 export const getImageUrl = (path: string): string => {
   if (!path) return "";
-  if (path.startsWith("http")) return path;
-  return `${SERVER_ORIGIN}${path}`;
+  return path;
 };
